f9fdbac937ef6cf81b74d72c370ecb44950872f1
splanes <sergi.planestor@gmail.com>
2020-11-22 14:24:56 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by splanes on 22/11/20.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
